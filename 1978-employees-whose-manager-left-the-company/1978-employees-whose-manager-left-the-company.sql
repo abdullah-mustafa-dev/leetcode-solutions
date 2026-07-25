ed009a299d9416aa1a86d9099b3c487e2b3ce39e
Abdullah Mustafa <abdallah.mostafa.akl@gmail.com>
2026-07-25 10:14:46 +0300

    Time: 200 ms (66.81%), Space: 0B (100%) - LeetHub

---
+++ 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
@@ -0,0 +1,8 @@
+-- Write your PostgreSQL query statement below
+select e.employee_id
+from Employees e
+where e.salary < 30000 and e.manager_id not in (
+    select employee_id
+    from Employees
+)
+order by e.employee_id;
