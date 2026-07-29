fb9fde1b9b0bcf46a4c5b6296ed4052476472e94
Abdullah Mustafa <abdallah.mostafa.akl@gmail.com>
2026-07-29 15:38:18 +0300

    Time: 307 ms (6.84%), Space: 0B (100%) - LeetHub

--- 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -2,7 +2,7 @@
 select p.product_name, sum(o.unit) as unit
 from Products p
 join Orders o on o.product_id = p.product_id
-WHERE o.order_date >= '2020-02-01'::date
-  AND o.order_date < '2020-02-01'::date + '1 month'::interval
+where o.order_date >= '2020-02-01'::date
+  and o.order_date < '2020-02-01'::date + '1 month'::interval
 group by product_name
 having sum(o.unit) >= 100;
